2a2816b6a42b2429a4b16f66646d909191f63c2d
Rob King <technology@escapecompass.com>
2014-12-06 15:01:10 -0800

    folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,6 +270,7 @@ module.exports = function (grunt) {
             // Copy moves asset files and directories.
             'img/**/*',
             'fonts/**/*',
+            'css/**/*',
             'CNAME',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
