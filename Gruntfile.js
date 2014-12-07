056557ae29ef560f35db251f4db1ef968eaa3843
Rob King <technology@escapecompass.com>
2014-12-06 22:23:12 -0800

    add to copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -277,6 +277,7 @@ module.exports = function (grunt) {
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
             'img/**/*',
+            'favicon.ico',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}'
